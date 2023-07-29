2cea8ee13582031540e80bd59e5fee62289cf0e1
Victor Hugo <victorneivadef8@gmail.com>
2023-07-28 21:27:15 -0300

    Busca por id e adição de produtos

--- src/main/java/com/prod_dept/prod_dept/controllers/ProductController.java
+++ src/main/java/com/prod_dept/prod_dept/controllers/ProductController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +22,34 @@ public class ProductController {
 	
 	@GetMapping
 	public List<Product> getObjects() {
-		
 		List<Product> result = productRepository.findAll();
 		return result;
-	
 	}
+	
+	
+	//Buscando produto pelo id
+	@GetMapping("/{id}")
+	public Product findById(@PathVariable Long id) {
+		Product result = productRepository.findById(id).get();
+		return result;
+	}
+	
+	
+	//Inserindo novo produto
+	@PostMapping
+	public Product inserir(@RequestBody Product product) {
+		Product result = productRepository.save(product);
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
